429c33eebf0374c8c101f1c2f7b47be2ffb7787f
Ilia <fortyhell@gmail.com>
2020-11-02 11:58:02 +0300

    First setup
    Add p5.js, delete unnecessary

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Sketch from "react-p5";
+import p5Types from "p5"; //Import this for typechecking and intellisense
+
 function App() {
+  let x = 50;
+  const y = 50;
+
+  //See annotations in JS for more information
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+      p5.createCanvas(500, 500).parent(canvasParentRef);
+  };
+
+  const draw = (p5: p5Types) => {
+      p5.background(0);
+      p5.ellipse(x, y, 70, 70);
+      x++;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sketch setup={setup} draw={draw} />
     </div>
   );
 }
