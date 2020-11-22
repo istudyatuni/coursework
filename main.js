407d7f18bf10c0599af3a24c78be5579bb9f2f52
Ilia <fortyhell@gmail.com>
2020-11-22 13:23:45 +0300

    Worked example

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-let canvas = document.querySelector('#c')
-let gl = canvas.getContext("webgl")
-if (!gl) {
-	console.log('WebGL not worked')
-} else {
-	console.log('WebGL worked')
-}
+/* eslint no-console:0 consistent-return:0 */
+"use strict";
 
 function createShader(gl, type, source) {
 	let shader = gl.createShader(type);   // создание шейдера
@@ -18,12 +13,6 @@ function createShader(gl, type, source) {
 	console.log(gl.getShaderInfoLog(shader));
 	gl.deleteShader(shader);
 }
-
-let vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
-let fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
-
-let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
 function createProgram(gl, vertexShader, fragmentShader) {
 	let program = gl.createProgram();
@@ -39,42 +28,77 @@ function createProgram(gl, vertexShader, fragmentShader) {
 	gl.deleteProgram(program);
 }
 
-let program = createProgram(gl, vertexShader, fragmentShader);
-let positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-let positionBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+function main() {
+	// Get A WebGL context
+	let canvas = document.querySelector("#c");
+	let gl = canvas.getContext("webgl");
+	if (!gl) {
+		return;
+	}
 
-// три двумерных точки
-var positions = [
-  0, 0,
-  0, 0.5,
-  0.7, 0,
-];
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+	// Get the strings for our GLSL shaders
+	let vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
+	let fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
 
-/*webglUtils.*/resizeCanvasToDisplaySize(gl.canvas);
-gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+	// create GLSL shaders, upload the GLSL source, compile the shaders
+	let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+	let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
-// очищаем canvas
-gl.clearColor(0, 0, 0, 0);
-gl.clear(gl.COLOR_BUFFER_BIT);
+	// Link the two shaders into a program
+	let program = createProgram(gl, vertexShader, fragmentShader);
 
-// говорим использовать нашу программу (пару шейдеров)
-gl.useProgram(program);
+	// look up where the vertex data needs to go.
+	let positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 
-// Привязываем буфер положений
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+	// Create a buffer and put three 2d clip space points in it
+	let positionBuffer = gl.createBuffer();
 
-// Указываем атрибуту, как получать данные от positionBuffer (ARRAY_BUFFER)
-var size = 2;          // 2 компоненты на итерацию
-var type = gl.FLOAT;   // наши данные - 32-битные числа с плавающей точкой
-var normalize = false; // не нормализовать данные
-var stride = 0;        // 0 = перемещаться на size * sizeof(type) каждую итерацию для получения следующего положения
-var offset = 0;        // начинать с начала буфера
-gl.vertexAttribPointer(
-    positionAttributeLocation, size, type, normalize, stride, offset)
+	// Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-var primitiveType = gl.TRIANGLES;
-var offset = 0;
-var count = 3;
-gl.drawArrays(primitiveType, offset, count);
+	// три двумерных точки
+	let positions = [
+		0, 0,
+		0, 0.5,
+		0.7, 0,
+	];
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+	// code above this line is initialization code.
+	// code below this line is rendering code.
+
+	webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+	// Tell WebGL how to convert from clip space to pixels
+	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+	// очищаем canvas
+	gl.clearColor(0, 0, 0, 0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+
+	// говорим использовать нашу программу (пару шейдеров)
+	gl.useProgram(program);
+
+	// Turn on the attribute
+	gl.enableVertexAttribArray(positionAttributeLocation);
+
+	// Привязываем буфер положений
+	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+	// Указываем атрибуту, как получать данные от positionBuffer (ARRAY_BUFFER)
+	let size = 2;          // 2 компоненты на итерацию
+	let type = gl.FLOAT;   // наши данные - 32-битные числа с плавающей точкой
+	let normalize = false; // не нормализовать данные
+	let stride = 0;        // 0 = перемещаться на size * sizeof(type) каждую итерацию для получения следующего положения
+	let offset = 0;        // начинать с начала буфера
+	gl.vertexAttribPointer(
+			positionAttributeLocation, size, type, normalize, stride, offset);
+
+	// draw
+	let primitiveType = gl.TRIANGLES;
+	offset = 0;
+	let count = 3;
+	gl.drawArrays(primitiveType, offset, count);
+}
+
+main();
